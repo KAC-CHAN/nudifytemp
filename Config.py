5323a60a9b60c2fe10905770ae081e5f2ba22e27
KAC CHAN <kacchan@outlook.in>
2024-08-16 10:58:40 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,7 +1,7 @@
 import os
 # Fillout The variables in Config.py further queries @riz4d 0n telegram
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6384743547:AAEol1WS5sbIthOx3Tz2OLLtBUW1CFaVzXA")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7403479954:AAFOKj3eEWG42NoCK3l-9S_YQZL-UMq-Vxg")
 API_ID = int(os.environ.get("API_ID", "26788480"))
 API_HASH = os.environ.get("API_HASH", "858d65155253af8632221240c535c314")
 
